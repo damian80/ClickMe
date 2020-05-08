9d1951024078059ac23663bb8a5479a685d689b1
Damian Ciasnocha <daneek80@gmail.com>
2020-05-08 21:56:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//div
+//.circle
+//button
+//targeting button
+const btn = document.querySelector("button");
+//console.log(btn);
+//listening and  name it a function
+btn.addEventListener("click", getNumber);
+//making a function
+function getNumber() {
+  let div = document.createElement("");
+  //console.log(div);
+}
